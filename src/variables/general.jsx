24a571bd029dd92f5253af68c8dbeb46d8a53913
aavanathan <arun_avanathan@intuit.com>
2019-04-20 21:49:47 -0700

    more changes

--- src/variables/general.jsx
+++ src/variables/general.jsx
@@ -2,33 +2,12 @@
 // // // table head data and table body data for Tables view
 // #############################
 
-const thead = ["Name", "Country", "City", "Salary"];
+const thead = ["Name", "Key", "Created", "Last Used"];
 const tbody = [
   {
     className: "table-success",
-    data: ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"]
+    data: ["Secure Key", "sk_test_abcdefghijklmnop123", "02/01/2019 13:34", "04/19/2019 12:34"]
   },
-  {
-    className: "",
-    data: ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"]
-  },
-  {
-    className: "table-info",
-    data: ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"]
-  },
-  {
-    className: "",
-    data: ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"]
-  },
-  {
-    className: "table-danger",
-    data: ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"]
-  },
-  { className: "", data: ["Mason Porter", "Chile", "Gloucester", "$78,615"] },
-  {
-    className: "table-warning",
-    data: ["Jon Porter", "Portugal", "Gloucester", "$98,615"]
-  }
 ];
 
 // data for <thead> of table in TableList view
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -31,7 +31,7 @@ class Dashboard extends React.Component {
           <Card>
               <CardHeader>
                 <CardTitle tag="h4">Keys</CardTitle>
-                <p className="card-category"> Here is a subtitle for this table</p>
+                <p> Please store these securely</p>
               </CardHeader>
               <CardBody>
                 <Table responsive>
@@ -68,99 +68,6 @@ class Dashboard extends React.Component {
                 </Table>
               </CardBody>
             </Card>  
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <Card>
-              <CardHeader>
-                <CardTitle>Users Behavior</CardTitle>
-                <p className="card-category">24 Hours performance</p>
-              </CardHeader>
-              <CardBody>
-                <Line
-                  data={dashboard24HoursPerformanceChart.data}
-                  options={dashboard24HoursPerformanceChart.options}
-                  width={400}
-                  height={100}
-                />
-              </CardBody>
-              <CardFooter>
-                <hr />
-                <Stats>
-                  {[
-                    {
-                      i: "fas fa-history",
-                      t: " Updated 3 minutes ago"
-                    }
-                  ]}
-                </Stats>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} sm={12} md={4}>
-            <Card>
-              <CardHeader>
-                <CardTitle>Email Statistics</CardTitle>
-                <p className="card-category">Last Campaign Performance</p>
-              </CardHeader>
-              <CardBody>
-                <Pie
-                  data={dashboardEmailStatisticsChart.data}
-                  options={dashboardEmailStatisticsChart.options}
-                />
-              </CardBody>
-              <CardFooter>
-                <div className="legend">
-                  <i className="fa fa-circle text-primary" /> Opened{" "}
-                  <i className="fa fa-circle text-warning" /> Read{" "}
-                  <i className="fa fa-circle text-danger" /> Deleted{" "}
-                  <i className="fa fa-circle text-gray" /> Unopened
-                </div>
-                <hr />
-                <Stats>
-                  {[
-                    {
-                      i: "fas fa-calendar-alt",
-                      t: " Number of emails sent"
-                    }
-                  ]}
-                </Stats>
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col xs={12} sm={12} md={8}>
-            <Card className="card-chart">
-              <CardHeader>
-                <CardTitle>NASDAQ: AAPL</CardTitle>
-                <p className="card-category">Line Chart With Points</p>
-              </CardHeader>
-              <CardBody>
-                <Line
-                  data={dashboardNASDAQChart.data}
-                  options={dashboardNASDAQChart.options}
-                  width={400}
-                  height={100}
-                />
-              </CardBody>
-              <CardFooter>
-                <div className="chart-legend">
-                  <i className="fa fa-circle text-info" /> Tesla Model S{" "}
-                  <i className="fa fa-circle text-warning" /> BMW 5 Series
-                </div>
-                <hr />
-                <Stats>
-                  {[
-                    {
-                      i: "fas fa-check",
-                      t: " Data information certified"
-                    }
-                  ]}
-                </Stats>
-              </CardFooter>
-            </Card>
           </Col>
         </Row>
       </div>
